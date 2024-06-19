8e2e6a31d5832beacfbbfe1bd99eba31489bdb13
Joe Blair <joemichaelblair@gmail.com>
2024-06-20 00:43:27 +0100

    Lil guy can now path around walls and go to destination clicked on the ground

--- objects/characters/lil_guy/lil_guy.gd
+++ objects/characters/lil_guy/lil_guy.gd
@@ -1,6 +1,10 @@
 extends CharacterBody3D
 
-
+var clickables : Array
+var current_target : Vector3
+var new_target : Vector3
+@export var marker_mesh : MeshInstance3D
+@onready var path_find : NavigationAgent3D = get_node("NavigationAgent3D")
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -9,5 +13,29 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+func _ready():
+	clickables = get_tree().get_nodes_in_group("clickable")
+	for clickable : CollisionObject3D in clickables:
+		clickable.input_event.connect(_on_click)
+
+
 func _physics_process(delta):
-	pass
+	if new_target != current_target:
+		path_find.target_position = new_target
+		current_target = new_target
+	
+	if not path_find.is_navigation_finished():
+		var path_pos = path_find.get_next_path_position()
+		look_at(path_pos)
+		velocity = -transform.basis.z * SPEED
+	else:
+		velocity = Vector3.ZERO
+	move_and_slide()
+	
+
+
+func _on_click(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		new_target = click_position
+		marker_mesh.global_position = click_position
+		print(click_position)
